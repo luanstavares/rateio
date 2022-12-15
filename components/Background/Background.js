6ff1fe845d0bb05cb39bf6922b5b9d6ed95dffd1
tavaresl <tavaresl@uni.coventry.ac.uk>
2022-12-15 17:23:00 +0000

    fixed background color to match figma

--- components/Background/Background.js
+++ components/Background/Background.js
@@ -3,8 +3,7 @@ import * as styles from "./Background.module.scss";
 export default function Background({ children }) {
   return (
     <div className={styles.bg}>
-      <div className={styles.bgGradient}></div>
-      <div className={styles.bgCircle}></div>
+      <div className={styles.bgColor}></div>
     </div>
   );
 }
