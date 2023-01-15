bc273b58c1bfe4973b00e82b1684ba8727e2b2f2
tavaresl <tavaresl@uni.coventry.ac.uk>
2023-01-15 12:08:26 +0000

    BEM implemented

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,25 +1,12 @@
 import * as styles from "./Logo.module.scss";
 import React from "react";
 
-export default function Logo({ style, size = 'medium' }) {
+export default function Logo({ size = "medium" }) {
   function styleClass() {
     const baseClass = `logo`;
     const sizeClass = `logo--${size}`;
 
-    return [styles[baseClass], styles[sizeClass]].join(' ');
-      case "logo__small":
-        return styles.logo__small;
-      case "logo__large":
-        return styles.logo__large;
-      case "logo__extra__large":
-        return styles.logo__extra__large;
-      case "logo__medium":
-      default:
-        return styles.logo__medium;
-    }
-  }
-  function logoClass() {
-    return `${styleClass()}`;
+    return [styles[baseClass], styles[sizeClass]].join(" ");
   }
 
   return <span className={styleClass()}>Rate.io</span>;
