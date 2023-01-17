d102856e733c708fbd4cf2cb292a1946798bcc6c
tavaresl <tavaresl@uni.coventry.ac.uk>
2023-01-17 12:15:48 +0000

    background deleted as material ui will be used for this purpose

--- components/Background/Background.js
+++
@@ -1,9 +0,0 @@
-import * as styles from "./Background.module.scss";
-
-export default function Background({ children }) {
-  return (
-    <div className={styles.background}>
-      <div className={styles.background__color}></div>
-    </div>
-  );
-}
