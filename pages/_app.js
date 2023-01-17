6a23d824e4ba62d8bc6392d20e2aec41d937b38d
tavaresl <tavaresl@uni.coventry.ac.uk>
2023-01-17 12:26:07 +0000

    material ui styles applied

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
 import "../styles/globals.scss";
+import {
+  createTheme,
+  responsiveFontSizes,
+  Paper,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  let theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#ffad33",
+        contrastText: "#000",
+      },
+      secondary: {
+        main: "#ffcc80",
+      },
+    },
+    typography: {
+      fontFamily: `Signika Negative`,
+      fontSize: 14,
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+      button: {
+        textTransform: "none",
+        lineHeight: 1.5,
+      },
+    },
+  });
+  theme = responsiveFontSizes(theme);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
