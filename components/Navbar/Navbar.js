008612201192c87958be4e80961b91f0556aa1dc
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-05-05 23:43:30 +0100

    starting Navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,11 +1,31 @@
+// Styles
 import * as styles from "../Navbar/Navbar.module.scss";
 
+// Material UI Components
+import { Grid, Container } from "@mui/material/";
+
+//Local Components
 import Logo from "../Logo/Logo";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import UserMenu from "../UserMenu/UserMenu";
 
 export default function Navbar() {
   return (
-    <div className={styles.navbar__logo}>
-      <Logo glow="active">Rate.io</Logo>
-    </div>
+    <Grid
+      height={100}
+      container
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center">
+      <Grid item>
+        <BurgerMenu />
+      </Grid>
+      <Grid item>
+        <Logo glow="active">Rate.io</Logo>
+      </Grid>
+      <Grid item>
+        <UserMenu />
+      </Grid>
+    </Grid>
   );
 }
