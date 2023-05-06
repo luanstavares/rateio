becb584bc2a20e1cdb2f51af82648f994fd05a9a
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-05-07 00:03:08 +0100

    created reusable drawer

---
+++ components/DrawerMenu/DrawerMenu.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Box, Drawer, IconButton } from "@mui/material";
+
+export default function DrawerMenu({ anchor, content, icon }) {
+  const [state, setState] = useState({ [anchor]: false });
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  return (
+    <>
+      <IconButton onClick={toggleDrawer(true)}>{icon}</IconButton>
+
+      <Drawer
+        anchor={anchor}
+        open={state[anchor]}
+        onClose={toggleDrawer(false)}
+      >
+        <Box
+          sx={{
+            marginTop: "10px",
+            width: 250,
+          }}
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          {content}
+        </Box>
+      </Drawer>
+    </>
+  );
+}
