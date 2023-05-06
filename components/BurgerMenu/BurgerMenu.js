d8b5b7af35ff7ac3b9b5f9bedc5276971ad93813
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-05-07 00:03:33 +0100

    created burger menu 🍔

--- components/BurgerMenu/BurgerMenu.js
+++ components/BurgerMenu/BurgerMenu.js
@@ -1,55 +1,16 @@
-import React, { useState } from "react";
-import { Box, Drawer, IconButton, useTheme } from "@mui/material/";
+import React from "react";
+import DrawerMenu from "../DrawerMenu/DrawerMenu";
+
 import { List } from "phosphor-react";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import MenuList from "../MenuList/MenuList";
 
-export default function DrawerMenu({ thisAnchor = "left" }) {
-  const theme = useTheme();
-
-  const [state, setState] = useState({
-    left: false,
-    right: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
-  const list = (anchor) => (
-    <Box
-      sx={{
-        marginTop: "10px",
-        width: 250,
-      }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      {anchor === "left" ? <MenuList /> : <h1>bye</h1>}
-    </Box>
-  );
-
+export default function BurgerMenu() {
   return (
     <>
-      <IconButton onClick={toggleDrawer(thisAnchor, true)}>
-        <List />
-      </IconButton>
-
-      <Drawer
-        anchor={thisAnchor}
-        open={state[thisAnchor]}
-        onClose={toggleDrawer(thisAnchor, false)}
-      >
-        {list(thisAnchor)}
-      </Drawer>
+      <DrawerMenu
+        content={<p>test burger menu</p>}
+        anchor="left"
+        icon={<List />}
+      />
     </>
   );
 }
