fd3dcb14eb8dc4aae6b3d3adab648f88945af8af
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-05-07 00:04:53 +0100

    created user menu 😄

--- components/UserMenu/UserMenu.js
+++ components/UserMenu/UserMenu.js
@@ -1,5 +1,15 @@
 import React from "react";
+import DrawerMenu from "../DrawerMenu/DrawerMenu";
+import { User } from "phosphor-react";
 
 export default function UserMenu() {
-  return <>UserMenu</>;
+  return (
+    <>
+      <DrawerMenu
+        content={<p>test user menu</p>}
+        anchor="right"
+        icon={<User />}
+      />
+    </>
+  );
 }
